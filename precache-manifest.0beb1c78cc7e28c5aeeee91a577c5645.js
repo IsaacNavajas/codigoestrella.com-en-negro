56772e3d31d00f391bf83db8f899e575dffe2c71
Your Name <you@example.com>
2021-08-14 10:40:29 +0200

    Updates

---
+++ precache-manifest.0beb1c78cc7e28c5aeeee91a577c5645.js
@@ -0,0 +1,402 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "305263b824d729b069fc1fd79c2315c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f837ae436703950b309",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "f2ad971526a2524f1407",
+    "url": "/static/css/main.3746ded6.chunk.css"
+  },
+  {
+    "revision": "1f837ae436703950b309",
+    "url": "/static/js/2.994035c4.chunk.js"
+  },
+  {
+    "revision": "28fa8ae9817b1f7a5dea7108e26b4348",
+    "url": "/static/js/2.994035c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2ad971526a2524f1407",
+    "url": "/static/js/main.85deb014.chunk.js"
+  },
+  {
+    "revision": "3c3f1816a6eeafdc4489",
+    "url": "/static/js/runtime-main.2ec26966.js"
+  },
+  {
+    "revision": "b474ae5353fcde9e88c28e64837774c6",
+    "url": "/static/media/admin_css_mu.b474ae53.txt"
+  },
+  {
+    "revision": "78584997c95937c8eb82df45025cef89",
+    "url": "/static/media/archivos.78584997.zip"
+  },
+  {
+    "revision": "49b3fc8cd83e09e9b1c8d1d35c30ae89",
+    "url": "/static/media/codezen 1.49b3fc8c.png"
+  },
+  {
+    "revision": "82931b6dc35ef21019c7c45f993a74c6",
+    "url": "/static/media/codezen 10.82931b6d.png"
+  },
+  {
+    "revision": "1cd951ad5550195a6e47f9a3b278d8d7",
+    "url": "/static/media/codezen 11.1cd951ad.png"
+  },
+  {
+    "revision": "a287a6f8130ce93823026276526ce985",
+    "url": "/static/media/codezen 12.a287a6f8.png"
+  },
+  {
+    "revision": "90df5efa2551b38231a3d82561961536",
+    "url": "/static/media/codezen 13.90df5efa.png"
+  },
+  {
+    "revision": "54a04eb4714c912a13bf0a72de439f0c",
+    "url": "/static/media/codezen 14.54a04eb4.png"
+  },
+  {
+    "revision": "b52c5ec879074126e41dc48ff1b60b31",
+    "url": "/static/media/codezen 16.b52c5ec8.png"
+  },
+  {
+    "revision": "ba8a3177ed3e91f7159625e4d7f549b2",
+    "url": "/static/media/codezen 17.ba8a3177.png"
+  },
+  {
+    "revision": "551cc9b5fc6d81afef278922c73eb12a",
+    "url": "/static/media/codezen 18.551cc9b5.png"
+  },
+  {
+    "revision": "9e5bddb225da9bca41583726d9ac4f83",
+    "url": "/static/media/codezen 19.9e5bddb2.png"
+  },
+  {
+    "revision": "36528f3fb9e7d962def847018741dc10",
+    "url": "/static/media/codezen 2.36528f3f.png"
+  },
+  {
+    "revision": "1b2972eb086b4cf9b550ea5f52fba78e",
+    "url": "/static/media/codezen 20.1b2972eb.png"
+  },
+  {
+    "revision": "6b1d471c0a682ad75894482ff1d99da8",
+    "url": "/static/media/codezen 3.6b1d471c.png"
+  },
+  {
+    "revision": "69b91aef13cbd384b25ed4564c83c271",
+    "url": "/static/media/codezen 4.69b91aef.png"
+  },
+  {
+    "revision": "2c66f1fffe1049ca6b12f8a0e37689a7",
+    "url": "/static/media/codezen 5.2c66f1ff.png"
+  },
+  {
+    "revision": "7d6a98f9cb1c67b3124d5375938f80fc",
+    "url": "/static/media/codezen 6.7d6a98f9.png"
+  },
+  {
+    "revision": "36b58313dc50af0f77a458e25700d2bb",
+    "url": "/static/media/codezen 7.36b58313.png"
+  },
+  {
+    "revision": "c10c43935a22f981a5e98ea3c4414a02",
+    "url": "/static/media/codezen 8.c10c4393.png"
+  },
+  {
+    "revision": "422bacc9e229ec679a842d41712c3523",
+    "url": "/static/media/codezen 9.422bacc9.png"
+  },
+  {
+    "revision": "187e4fd6a8322dc3be2f4a583571afbd",
+    "url": "/static/media/codezen Vue_1.187e4fd6.png"
+  },
+  {
+    "revision": "0389206f68d0c58b422d7da27bcfe52e",
+    "url": "/static/media/codezen Vue_2.0389206f.png"
+  },
+  {
+    "revision": "f4bd16fae3c9b4e43a1f30f46dc2987d",
+    "url": "/static/media/codezen bookly 1.f4bd16fa.png"
+  },
+  {
+    "revision": "1681bbc584d56fb434af7db09aa0b028",
+    "url": "/static/media/codezen bookly 2.1681bbc5.png"
+  },
+  {
+    "revision": "94185199c2750a44c26976a9e996415d",
+    "url": "/static/media/codezen bookly 3.94185199.png"
+  },
+  {
+    "revision": "3d8e31e8b94a2644d7cab3da4dd38d91",
+    "url": "/static/media/codezen bookly 4.3d8e31e8.png"
+  },
+  {
+    "revision": "77726c7c2a67bdbd0c01e263f80c8944",
+    "url": "/static/media/codezen callbacks.77726c7c.png"
+  },
+  {
+    "revision": "57a21084e1d2d2cb49333e8b47aff9c3",
+    "url": "/static/media/codezen docker 1.57a21084.png"
+  },
+  {
+    "revision": "0adafa8441f94af91e08a741afbbbdfb",
+    "url": "/static/media/codezen docker 2.0adafa84.png"
+  },
+  {
+    "revision": "87a8da6418567f872c5305a44dc0ca94",
+    "url": "/static/media/codezen multisite1.87a8da64.png"
+  },
+  {
+    "revision": "dbb3f4a5df6e5ed86269a1ab47aef045",
+    "url": "/static/media/codezen multisite11.dbb3f4a5.jpg"
+  },
+  {
+    "revision": "c877a418da33f2557681f6ea985652f2",
+    "url": "/static/media/codezen multisite12.c877a418.png"
+  },
+  {
+    "revision": "8f3559ed8d706253343d3dbfa457735c",
+    "url": "/static/media/codezen multisite13.8f3559ed.png"
+  },
+  {
+    "revision": "25cd7ed82b8773da73c9546bcaf31f07",
+    "url": "/static/media/codezen multisite14.25cd7ed8.png"
+  },
+  {
+    "revision": "8551cfc2c6f644f0e5845543949a0944",
+    "url": "/static/media/codezen multisite16.8551cfc2.png"
+  },
+  {
+    "revision": "a91a173025f473a418b27d86ddffbf27",
+    "url": "/static/media/codezen multisite17.a91a1730.png"
+  },
+  {
+    "revision": "1b2db4660f0b92acee7c8aac0c6e67ac",
+    "url": "/static/media/codezen multisite18.1b2db466.png"
+  },
+  {
+    "revision": "7b09e8fafa81b5481bad3ff5e4b277f2",
+    "url": "/static/media/codezen multisite2.7b09e8fa.png"
+  },
+  {
+    "revision": "33b5407f638295b28fe7765d4bd6d7ef",
+    "url": "/static/media/codezen multisite3.33b5407f.png"
+  },
+  {
+    "revision": "dddc993e829098eb13d934eb2114432e",
+    "url": "/static/media/codezen multisite4.dddc993e.png"
+  },
+  {
+    "revision": "4c95442b81f09d3f0de9610626ed081a",
+    "url": "/static/media/codezen multisite5.4c95442b.png"
+  },
+  {
+    "revision": "c4d911d1a28c0ba52543619a573514fb",
+    "url": "/static/media/codezen multisite7.c4d911d1.jpg"
+  },
+  {
+    "revision": "b9c8d7ab8dd8a78de7075292a41b0328",
+    "url": "/static/media/codezen multisite8.b9c8d7ab.png"
+  },
+  {
+    "revision": "55b3c8420b6b1df36bf647d130056294",
+    "url": "/static/media/codezen multisite9.55b3c842.png"
+  },
+  {
+    "revision": "663edb0d63e8b85fecbaf55c5e8fcb89",
+    "url": "/static/media/codezen promesas.663edb0d.png"
+  },
+  {
+    "revision": "3d31f05a7710222b8b2fcb6e27cb64a8",
+    "url": "/static/media/codezen static.3d31f05a.png"
+  },
+  {
+    "revision": "df0846938134c80553447c06ae59be53",
+    "url": "/static/media/codezenApiJs.df084693.png"
+  },
+  {
+    "revision": "577ff2e63bc72d7c008b3ac3b86633f4",
+    "url": "/static/media/codezenElectron.577ff2e6.png"
+  },
+  {
+    "revision": "db90baec7c48c43b43f0fb476a793adb",
+    "url": "/static/media/codezenExpressDos.db90baec.png"
+  },
+  {
+    "revision": "e8c3bbcbc42f67c2658ef5b66d122732",
+    "url": "/static/media/codezenExpressUno.e8c3bbcb.png"
+  },
+  {
+    "revision": "8a76e9a83bb789a1b2fa10cc45e9358e",
+    "url": "/static/media/codezenHomeJs.8a76e9a8.png"
+  },
+  {
+    "revision": "1499f9d5c8e8544a75c16f9c0dff5a78",
+    "url": "/static/media/codezenMongodbCuatro.1499f9d5.png"
+  },
+  {
+    "revision": "eb5b98e779552ea04a4420775a899846",
+    "url": "/static/media/codezenMongodbDos.eb5b98e7.png"
+  },
+  {
+    "revision": "a680b7e6961a7315a5f6bd490c221da5",
+    "url": "/static/media/codezenMongodbTres.a680b7e6.png"
+  },
+  {
+    "revision": "9320d0fc4e7f551e62e59bbfaa6fe0ce",
+    "url": "/static/media/codezenMongodbUno.9320d0fc.png"
+  },
+  {
+    "revision": "06fadeb32c260b148f5729ff2b8f5907",
+    "url": "/static/media/codezenPxAssetsTableVue.06fadeb3.png"
+  },
+  {
+    "revision": "c8e69a0b8469101a2ae64b12c6ad5e45",
+    "url": "/static/media/codezenVueProps1.c8e69a0b.png"
+  },
+  {
+    "revision": "35600012dda49a30bf37eb10e9eeaef2",
+    "url": "/static/media/codezenVueProps2.35600012.png"
+  },
+  {
+    "revision": "8363c0ae68bf777c9c16a75acf3e93b6",
+    "url": "/static/media/codezenVueProps3.8363c0ae.png"
+  },
+  {
+    "revision": "578893369f90848561e18ce9ca51d7b5",
+    "url": "/static/media/codezenVueProps4.57889336.png"
+  },
+  {
+    "revision": "5eccf8713662f40da12d1d16ed6752cd",
+    "url": "/static/media/codezen_codigo_wordpress.5eccf871.png"
+  },
+  {
+    "revision": "d8ead7b05eabfe252bbae786beb4d92d",
+    "url": "/static/media/diagramadevenn1.d8ead7b0.png"
+  },
+  {
+    "revision": "526b07356b3eb850ed0dcaf486988a32",
+    "url": "/static/media/diagramadevenn2.526b0735.png"
+  },
+  {
+    "revision": "4f86454ca39c7b47c59281ca1d9ee1ea",
+    "url": "/static/media/diagramadevenn3.4f86454c.png"
+  },
+  {
+    "revision": "b91209a10397b1ff8dd7c4239bb40874",
+    "url": "/static/media/diagramadevenn4.b91209a1.png"
+  },
+  {
+    "revision": "169d627c85073ab93b5a09e80e1a09c3",
+    "url": "/static/media/diagramadevenn5.169d627c.png"
+  },
+  {
+    "revision": "3118cde3ee293f2c024c417b9a202afd",
+    "url": "/static/media/image 1.3118cde3.png"
+  },
+  {
+    "revision": "14fac1e8bf3233aa9cc949e645068861",
+    "url": "/static/media/image elementor.14fac1e8.png"
+  },
+  {
+    "revision": "9157fe3feb229797bd5f807c9ef6ade2",
+    "url": "/static/media/image elementor2.9157fe3f.png"
+  },
+  {
+    "revision": "db2be05c5f9dc5b28bed3cc99a80e251",
+    "url": "/static/media/image elementor3.db2be05c.png"
+  },
+  {
+    "revision": "bd41e66a0e1c5837ca15ae5898aaa732",
+    "url": "/static/media/image html.bd41e66a.png"
+  },
+  {
+    "revision": "3d9495be09aa221ee60dd1119c0e89d0",
+    "url": "/static/media/image javascript.3d9495be.png"
+  },
+  {
+    "revision": "626c04b5577a31f52c166bf1ab9e180b",
+    "url": "/static/media/imageTCP.626c04b5.png"
+  },
+  {
+    "revision": "74c6bf105279be76b26fb5285a4385aa",
+    "url": "/static/media/imagedocker.74c6bf10.png"
+  },
+  {
+    "revision": "90a4d1d86fb9999bd985cc89fe4c4cdd",
+    "url": "/static/media/imagenExplicacionDNS.90a4d1d8.png"
+  },
+  {
+    "revision": "62a93333ccf513f66e124f64f16e460e",
+    "url": "/static/media/logo code.62a93333.png"
+  },
+  {
+    "revision": "0e4fdce8ac22e09688c580e5bc4dcd7d",
+    "url": "/static/media/logo js.0e4fdce8.png"
+  },
+  {
+    "revision": "b187e3b1985b0aa738093d97ce028ab6",
+    "url": "/static/media/logo php.b187e3b1.png"
+  },
+  {
+    "revision": "c6676c5fce75797727c65f4fec4ee115",
+    "url": "/static/media/logo-mysql.c6676c5f.png"
+  },
+  {
+    "revision": "892c615597201703135e947c65dec2b5",
+    "url": "/static/media/logo-prestashop.892c6155.png"
+  },
+  {
+    "revision": "60d4cba16a2e44b6338c35af050fa146",
+    "url": "/static/media/logo-wordpress.60d4cba1.png"
+  },
+  {
+    "revision": "e4855bf25d732863cce1c63f830e24ec",
+    "url": "/static/media/mysqlTabla.e4855bf2.png"
+  },
+  {
+    "revision": "78862327be5506c584b855778133e3a9",
+    "url": "/static/media/prettierrc.78862327.png"
+  },
+  {
+    "revision": "20ce066170bd2641173e90c7b1f39150",
+    "url": "/static/media/spam.20ce0661.png"
+  },
+  {
+    "revision": "731426fe72ec57a2100b474a0a7a769d",
+    "url": "/static/media/tuplas.731426fe.png"
+  },
+  {
+    "revision": "5313215f8f59a0f4d776b994c696db20",
+    "url": "/static/media/tuplasdos.5313215f.png"
+  },
+  {
+    "revision": "0bab56b4d475613a691d6c1cd2068635",
+    "url": "/static/media/wordpresspermisos2.0bab56b4.png"
+  },
+  {
+    "revision": "bb3368cf3a0747a98e660eab2c96a213",
+    "url": "/static/media/wordpresspermisos3.bb3368cf.png"
+  },
+  {
+    "revision": "2886dae1251d4f5b77d28620f81b7d75",
+    "url": "/static/media/wordpresspermisos4.2886dae1.png"
+  },
+  {
+    "revision": "493b5268154c5d1c0d2e6bbbd320e771",
+    "url": "/static/media/wordpresspermisos5.493b5268.png"
+  },
+  {
+    "revision": "6de1302c97b581b533ed494351fd410b",
+    "url": "/static/media/wordpresspermisos6.6de1302c.png"
+  },
+  {
+    "revision": "e9b7190c982efb7e32e060f9e4aba644",
+    "url": "/static/media/wordpresspermisos7.e9b7190c.png"
+  }
+]);
